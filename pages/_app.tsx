061067a441d7e8f9a9d5afbec83a8b4f35e0ff28
Mayur Padshala <mayur.padshala@primelab.io>
2022-02-21 19:46:24 +0530

    Add and configure dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
+/**
+ * Home: The Landing page of the web app
+ * @return {JSX.Element} The JSX Code for the Home Page
+ */
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
