727dcfe6358b1af9d824c500082f3110ad8f4b24
Mayur Padshala <mayur.padshala@primelab.io>
2022-02-23 17:53:37 +0530

    Fix tests after adding provider

--- pages/home/index.test.tsx
+++ pages/home/index.test.tsx
@@ -1,9 +1,15 @@
+import { store } from '@/store/index';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import Home from './index.page';
 
 describe('Home', () => {
   it('renders a welcome message', () => {
-    render(<Home />);
+    render(
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    );
 
     const heading = screen.getByRole('heading', {
       name: /Welcome to Scaffold/i,
